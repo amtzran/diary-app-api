14a86e0387823ad0720d1f01924bfa33f82bc14c
Alberto Martínez <amtzran@gmail.com>
2023-03-02 08:01:08 -0600

    add base model

--- app/base/models.py
+++ app/base/models.py
@@ -1,3 +1,23 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    """Base model for create other system models."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    is_active = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_deleted = True
+        self.save()
+
+    def hard_delete(self):
+        super(BaseModel, self).delete()
