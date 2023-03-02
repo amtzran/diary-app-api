51d9e4cac4f9fb0c93dd76607aed54c4d3ce17a7
Alberto Martínez <amtzran@gmail.com>
2023-03-02 08:06:56 -0600

    Add authentication with jwt

---
+++ app/users/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework.serializers import ModelSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+from users.models import User
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        exclude = ['is_deleted', 'groups', 'user_permissions', 'confirmation_code']
+
+    def create(self, validated_data):
+        user = User.objects.create(**validated_data)
+        user.set_password(validated_data['password'])
+        user.is_active = True
+        user.is_verified = True
+        user.save()
+        return user
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data.pop('password', None)
+        return data
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def update(self, instance, validated_data):
+        pass
+
+    def create(self, validated_data):
+        pass
--- app/users/views.py
+++ app/users/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from base.pagination import StandardResultsSetPagination
+from base.response import *
+from users.serializers import *
+
+
+class UserViewSet(ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = UserSerializer.Meta.model.objects.order_by('-created_at').filter(is_deleted=False)
+    pagination_class = StandardResultsSetPagination
+    permission_classes = [IsAuthenticated]
+
+
+class Login(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username', '')
+        password = request.data.get('password', '')
+
+        user = authenticate(username=username, password=password)
+        data = {'username': username, 'password': password}
+
+        if user:
+            login_serializer = self.serializer_class(data=data)
+            if login_serializer.is_valid():
+                user_serializer = UserSerializer(user)
+                data = {
+                    'access_token': login_serializer.validated_data.get('access'),
+                    'refresh_token': login_serializer.validated_data.get('refresh'),
+                    'type': 'Bearer',
+                    'user': user_serializer.data
+                }
+                return response_success(data=data, message='Bienvenido, sus credenciales son correctas.')
+            return response_error(message='Las credenciales son incorrectas.')
+        return response_error(message='Las credenciales son incorrectas.')
+
+
+class Logout(GenericAPIView):
+    @classmethod
+    def post(cls, request, *args, **kwargs):
+        user = User.objects.filter(id=request.data.get('user', 0))
+        if user.exists():
+            RefreshToken.for_user(user.first())
+            return response_success(message='Sesión cerrada correctamente.')
+        return response_not_found(message='No existe este usuario')
