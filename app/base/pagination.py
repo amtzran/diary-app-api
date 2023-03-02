699a5fbf1a144705efdbad1fbff4b287cdf3b831
Alberto Martínez <amtzran@gmail.com>
2023-03-02 08:03:00 -0600

    add custom pagination and response base

---
+++ app/base/pagination.py
@@ -0,0 +1,21 @@
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+
+class LargeResultsSetPagination(PageNumberPagination):
+    page_size = 1000
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
+
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
+
+    def get_paginated_response(self, data):
+        return Response({
+            'total': self.page.paginator.count,
+            'page': self.page.number,
+            'page_size': int(self.request.query_params['page_size']),
+            'items': data
+        })
